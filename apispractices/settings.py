8ed5dc3796630309942a72a98297e6dd557a782b
victor <victormagallanes2@gmail.com>
2023-05-17 23:29:20 -0400

    delete crud

--- apispractices/settings.py
+++ apispractices/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    #'apispractices.crud'
-    'crud'
+    'users'
 
 ]
 
@@ -125,3 +124,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
